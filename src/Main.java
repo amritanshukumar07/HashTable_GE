0b07050ca1509fcc871ac8910425fb49c2642774
Amritanshu Kumar <amritanshu8271@gmail.com>
2024-12-30 20:37:06 +0530

    modification for paragraph handling

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to day 10 HashTable Program ");
-        String sentence= "To be or not to be";
-        String[] words= sentence.toLowerCase().split(" ");
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] words = paragraph.toLowerCase().split(" ");
         HashTable<String,Integer>map= new HashTable<>();
 
         // calculate freq of each word
